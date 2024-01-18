1a570ed762af87e8e5ffdfb20c577aaeb953f63c
Brandon Apuntar <bapuntar@ucsc.edu>
2024-01-17 23:23:29 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// Code Practice: Making a Scene
+// Name: Brandon Apuntar
+// Date: 1/17/24
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [MainMenu, Play]
+
+}
+
+let game = new Phaser.Game(config)
